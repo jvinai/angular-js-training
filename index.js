440022bad5edb7257618d729c692802c15582d0c
Lammert Westerhoff <westerhoff@gmail.com>
2014-06-23 12:02:57 +0200

    Write package information to html file

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-var packageJSON = require('./package.json');
-console.log('Running ' + packageJSON.name + ' version ' + packageJSON.version);
+var fs = require('fs');
+
+fs.readFile('package.json', {encoding: 'UTF-8'}, function (err, data) {
+  var packageJSON = JSON.parse(data);
+
+  function listItem(key, value) {
+    return '<li><strong>' + key + ': </strong>' + value + '</li>';
+  }
+  var html = '<ul>' +
+    listItem('Name', packageJSON.name) +
+    listItem('Version', packageJSON.version) +
+    '</ul>';
+
+  fs.writeFile('./tmp/package.html', html, function (err) {
+    if (err) {
+      console.error('Failed to write html to file', err);
+    } else {
+      console.log('Written html to tmp/package.html')
+    }
+  });
+});
