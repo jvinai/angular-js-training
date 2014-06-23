f2198d8d449d1a74dbb5fc9ebe01a95a6a0b15ba
Lammert Westerhoff <westerhoff@gmail.com>
2014-06-23 14:11:13 +0200

    Use options in html task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,31 @@ var phtml = require('./lib/packageHtml.js')
 function packageHtml() {
   var done = this.async();
 
-  fs.readFile('package.json', {encoding: 'UTF-8'}, function (err, data) {
-    var packageJSON = JSON.parse(data);
+  var options = this.options();
 
-    var html = phtml(packageJSON);
+  var html = phtml(options.name, options.version);
 
-    fs.writeFile('./tmp/package.html', html, function (err) {
-      if (err) {
-        console.error('Failed to write html to file', err);
-      } else {
-        console.log('Written html to tmp/package.html')
-      }
-      done();
-    });
+  fs.writeFile(options.file, html, function (err) {
+    if (err) {
+      console.error('Failed to write html to file', err);
+    } else {
+      console.log('Written html to ' + options.file);
+    }
+    done();
   });
 }
 
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    package: grunt.file.readJSON('package.json'),
+    html: {
+      options: {
+        file: './tmp/package.html',
+        name: '<%= package.name %>',
+        version: '<%= package.version %>'
+      }
+    },
     htmlangular: {
       files: {
         src: ['tmp/*.html'],
