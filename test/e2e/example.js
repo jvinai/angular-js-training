bb4085b6e032d3e3bfe8effcf9fd0be55ba8e419
Joshua Appelman <jappelman@xebia.com>
2014-06-29 22:53:36 +0200

    Adds a test for posting thoughts.

--- test/e2e/example.js
+++ test/e2e/example.js
@@ -1,5 +1,31 @@
-describe('the e2e test', function () {
-	it('should open the page', function () {
-		expect(true).toBe(true);
-	})
+describe('the index page', function () {
+
+	var initialNumberOfThoughts;
+
+	it('should navigate to the index page', function () {
+		browser.get('');
+	});
+
+	it('should initially have a number of thoughts on the page', function () {
+		var thoughts = element.all(by.repeater("thought in thoughts"));
+		var promiseToCount = thoughts.count();
+
+		promiseToCount.then(function (n) {
+			initialNumberOfThoughts = n;
+		});
+
+		expect(promiseToCount).toBeGreaterThan(0);
+	});
+
+	it('should allow the posting of a thought', function () {
+		var inputField = element(by.model('newThought'));
+		inputField.sendKeys('Who is this Rorschach guy and why does he paint so many pictures of my parents fighting?');
+		element(by.css('.btn-primary')).click();
+	});
+
+	it('should then display this thought, after submitting', function () {
+		expect(element.all(by.repeater("thought in thoughts")).count()).toBe(initialNumberOfThoughts + 1);
+		expect(element.all(by.repeater("thought in thoughts")).get(0).getText()).toEqual('Who is this Rorschach guy and why does he paint so many pictures of my parents fighting?');
+	});
+	
 })
